ca1145f15b2359d1ae2ba00b1c03029041dce507
Bryan Siegel <bryansiegel@gmail.com>
2020-04-10 08:59:56 -0700

    simple url changes

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def welcome(request):
     return HttpResponse('Welcome To the Meeting Planner')
+
+
+def date(request):
+    return HttpResponse('This page was served at' + str(datetime.now()))
+
