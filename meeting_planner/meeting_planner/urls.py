69348d8e713c86db2b7609d28bc227ebc5a8a1f9
Bryan Siegel <bryansiegel@gmail.com>
2020-04-13 09:45:56 -0700

    urls refactor

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from meetings.views import detail, rooms
 from website.views import welcome, date, about
 
 urlpatterns = [
@@ -24,6 +23,6 @@ urlpatterns = [
     path('', welcome, name='welcome'),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', detail, name="detail"),
-    path('meetings/rooms', rooms, name='rooms')
+    path('meetings/', include('meetings.urls'))
+
 ]
